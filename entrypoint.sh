6dce9ac08009cd32d96685662f2a9f3d475dba17
Teem KWONG <mdskwong@gmail.com>
2026-05-13 17:05:11 -0700

    Add entrypoint shell script for Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Starting Django Server on port 8000..."
+python app/manage.py runserver 0.0.0.0:8000
+
+echo "Starting Jupyter Lab on port 8888..."
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' &
