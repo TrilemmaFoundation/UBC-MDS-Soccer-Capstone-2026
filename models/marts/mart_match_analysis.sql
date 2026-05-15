aa2878944052cebdc75153afe048a9fb5ffa0c5b
Teem KWONG <mdskwong@gmail.com>
2026-05-14 22:58:23 -0700

    Create mart model stubs

---
+++ models/marts/mart_match_analysis.sql
@@ -0,0 +1,4 @@
+{{ config(materialized='table') }}
+
+-- Final view for Match Overview dashboard page
+SELECT 1 as placeholder
---
+++ models/marts/mart_player_performance.sql
@@ -0,0 +1,4 @@
+{{ config(materialized='table') }}
+
+-- Final view for Player Performance dashboard page
+SELECT 1 as placeholder
