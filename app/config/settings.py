98431b5e07726d03cb68ca277f299e55522d7775
Quan Hoang <72828634+quandothoang@users.noreply.github.com>
2026-06-17 23:44:35 -0700

    fixed some naming

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,7 +14,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # Security
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
